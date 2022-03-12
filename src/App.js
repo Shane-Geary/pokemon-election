c87088c10065fd9383951220dd71f5ba344c2c1e
Shane Geary <shane.geary@glowstik.com>
2022-03-12 10:06:42 -0700

    Title and added CurrentWinner component for side-panel

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import CurrentWinner from './CurrentWinner';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>
+        Pokemon Election
+      </h1>
+      <div className='header-line'>
+        <CurrentWinner/>
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/CurrentWinner.js
@@ -0,0 +1,12 @@
+import './App.css'
+
+function CurrentWinner() {
+
+    return (
+        <div className='drawer-wrapper'>
+          
+        </div>
+    )
+}
+
+export default CurrentWinner
