b4304d1d4fe0e286da3e365724997a7de448eab1
Shane Geary <shane.geary@glowstik.com>
2022-03-12 17:48:26 -0700

    Mapping through json returned pokemon data and rendering sprites

--- src/PokemonGrid.js
+++ src/PokemonGrid.js
@@ -1,36 +1,21 @@
 import { useState, useEffect } from 'react'
 import './Grid.css'
-import { useQuery } from "react-query";
-import PokemonThumbnail from './PokeThumbnail'
+import { useQuery } from "react-query"
 
 function PokemonGrid() {
 
-    // const [pokedex, setPokedex] = useState([])
-
-    // useEffect(() => {
-    //     const fetchData = async () => {
-    //         const resp = await fetch(
-    //             'https://pokeapi.co/api/v2/pokemon?limit=20'
-    //             )
-    //         const data = await resp.json()
-    //         setPokedex(data.resp)
-    //     }
-    //     console.log(pokedex)
-    //     fetchData()
-    // }, [])
-
     const getPokemons = async () => {
-        const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=20");
-        const data = await response.json();
+        const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=20")
+        const data = await response.json()
       
-        return data;
+        return data
       };
       
       const getPokemon = async (url) => {
-        const response = await fetch(url);
-        const data = await response.json();
+        const response = await fetch(url)
+        const data = await response.json()
       
-        return data;
+        return data
       };
       
       const PokemonTile = ({ name, url }) => {
@@ -39,7 +24,7 @@ function PokemonGrid() {
         );
       
         if (error) {
-          return <div>Error: {error}</div>;
+          return <div>Error: {error}</div>
         }
       
         if (isLoading) {
@@ -60,16 +45,28 @@ function PokemonGrid() {
         );
       };
 
+      const { error, isLoading, data } = useQuery("pokemons", getPokemons);
+
+      if (error) {
+        return <div>Error: {error}</div>
+      }
+    
+      if (isLoading) {
+        return (
+            <div>Loading...</div>
+        )
+      }
+    
+      const { results: pokemons } = data
+
     return (
         <div>
             <div className='grid-wrapper'>
                 <div className='id1'>
                     <div>
-                        {/* {pokedex.map(({name, url}) => (
-                            <div key={url}>
-                                {name}
-                            </div>
-                        ))} */}
+                    {pokemons.map((pokemon) => (
+                        <PokemonTile key={pokemon.name} {...pokemon} />
+                    ))}
                     </div>
                 </div>
             </div>
