38ae334dda7bab5fb28ac8f7f7491aba06fa9f1d
Shane Geary <shane.geary@glowstik.com>
2022-04-13 06:31:41 -0600

    Updated to latest version of React and wrapped app at the highest level in the DndProvider HTML5Backend from react dnd

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import './App.css'
 // import CurrentWinner from './CurrentWinner'
 import PokeGrid from './PokeGrid'
+
+import {DndProvider} from 'react-dnd'
+import {HTML5Backend} from 'react-dnd-html5-backend'
  
 function App() {
   return (
-    <div className="App">
-      <PokeGrid />
-    </div>
+    <DndProvider backend={HTML5Backend}>
+      <div className="App">
+        <PokeGrid />
+      </div>
+    </DndProvider>
   )
 }
 
